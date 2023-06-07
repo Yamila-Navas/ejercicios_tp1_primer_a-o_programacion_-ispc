56ec4592e632bbd3f7d9a7bf63bce096acb9a320
Yamila-Navas <yaminavassole@hotmail.com>
2023-06-07 20:28:13 -0300

    solucion al ejercicio uno

---
+++ 1.py
@@ -0,0 +1,7 @@
+primeraLetra = input("Digite la primer letra--> ")
+segundaLetra = input("Digite la sugunda letra--> ")
+
+if primeraLetra == segundaLetra:
+    print("las letras son igules")
+else:
+    print("las letras no son iguales")
